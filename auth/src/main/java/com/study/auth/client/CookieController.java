e04766fd4dacb9e82d2897dad9ad4e1d0d020900
huanglinchun <huanglinchun@runjingchina.com>
2023-10-19 14:59:17 +0800

    鉴权服务

---
+++ auth/src/main/java/com/study/auth/client/CookieController.java
@@ -0,0 +1,11 @@
+package com.study.auth.client;
+
+/**
+ * @author : huanglinchun
+ * @description:
+ * @date : Created in 2023/10/19
+ * @modified By: huanglinchun
+ * @project: spring_cloud_study
+ */
+public class CookieController {
+}
